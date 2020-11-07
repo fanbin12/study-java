ea6f4e826064d1023519037102812b35af5cb630
fanbin12 <1523294515@qq.com>
2020-11-07 19:19:02 +0800

    这是一个案例

---
+++ Http&Servlet/fanbin-servlet/test/src/cn/itcast/util/JDBCutil.java
@@ -0,0 +1,40 @@
+package cn.itcast.util;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class JDBCutil {
+    private  static DataSource ds;
+    static {
+        //加载配置文件
+        Properties properties = new Properties();
+        //使用classloader加载配置文件，获取字节输入流
+        InputStream resourceAsStream = JDBCutil.class.getClassLoader().getResourceAsStream("druid.properties");
+        try {
+            properties.load(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //初始化连接池对象
+        try {
+            ds = DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+    * h获取连接池对象*/
+    public  static  DataSource getDataSource(){
+        return ds;
+    }
+    /*获取connection对象*/
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
